459c23515ad0057e6696ac1e216070af9990669f
Guimill <guillaume.milleret@gmail.com>
2023-04-05 20:39:05 +0200

    corrections diverses

--- src/components/data/realisationsData.js
+++ src/components/data/realisationsData.js
@@ -4,13 +4,13 @@ import shiny from '../../assets/img/shiny.png';
 export const RealisationsData = [
     {
         "id": 1,
-        "src": lisa,
+        "src": shiny,
         "caption": "Synthetiseur de fonctionnalites des zones humides",
         "href": "https://milleretg4progress.shinyapps.io/shinyapps/"
     },
     {
         "id": 2,
-        "src": shiny,
+        "src": lisa,
         "caption": "The Simpsons Politiscale",
         "href": "https://the-simpsons-politiscales.fr/"
     }
