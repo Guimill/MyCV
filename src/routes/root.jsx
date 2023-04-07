304e5b66610d89a48d1256104002c1f46f1709cd
Guimill <guillaume.milleret@gmail.com>
2023-04-07 15:51:16 +0200

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,10 @@
-import Header from '../components/header'
+import Header from '../components/header';
 import Navbar from '../components/navbar';
 import { Outlet } from 'react-router-dom';
-import Footer from '../components/footer/index'
+import Footer from '../components/footer/index';
 
 export default function Root() {
+
   return (
     <>
       <Header />
