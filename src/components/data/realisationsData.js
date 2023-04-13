f91b02ce71d6c0d4fe8a7bcfc2f8cbf6ee5d0532
Guimill <guillaume.milleret@gmail.com>
2023-04-13 12:03:14 +0200

    new realisation old cv

--- src/components/data/realisationsData.js
+++ src/components/data/realisationsData.js
@@ -1,6 +1,7 @@
 import lisa from '../../assets/img/lisa.png';
 import shiny from '../../assets/img/shiny.png';
-import MPC from '../../assets/img/isla_500x500.54939114_c40i6j8i.webp'
+import MPC from '../../assets/img/isla_500x500.54939114_c40i6j8i.webp';
+import OldCV from '../../assets/img/oldcv.jpeg'
 
 export const RealisationsData = [
     {
@@ -20,5 +21,11 @@ export const RealisationsData = [
         "src": MPC,
         "caption": "Mon petit coton",
         "href": "https://mon-petit-coton.netlify.app/gallery"
+    },
+    {
+        "id": 4,
+        "src": OldCV,
+        "caption": "Ancien CV",
+        "href": "https://www.linkedin.com/in/guillaume-milleret/"
     }
 ]
