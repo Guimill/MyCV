18fc991caf7260bd41dbc69852045c29bcc3c3f5
Guimill <86968380+Guimill@users.noreply.github.com>
2023-05-03 19:11:32 +0200

    Add a new realisation

--- src/components/data/realisationsData.js
+++ src/components/data/realisationsData.js
@@ -1,29 +1,36 @@
 import lisa from '../../assets/img/lisa.png';
-import shiny from '../../assets/img/shiny.png';
+import shinyZH from '../../assets/img/shiny.png';
 import MPC from '../../assets/img/isla_500x500.54939114_c40i6j8i.webp';
 import OldCV from '../../assets/img/oldcv.jpeg'
+import ShinyLeaflet from '../../assets/img/ShinyLeaflet'
 
 export const RealisationsData = [
     {
         "id": 1,
-        "src": shiny,
+        "src": ShinyLeaflet,
+        "caption": "Cartographie des la population française par département en 2023",
+        "href": "https://milleretg4progress.shinyapps.io/R-leaflet-Fr-pop-data/"
+    },
+    {
+        "id": 2,
+        "src": shinyZH,
         "caption": "Synthetiseur de fonctionnalites des zones humides",
         "href": "https://milleretg4progress.shinyapps.io/shinyapps/"
     },
     {
-        "id": 2,
+        "id": 3,
         "src": lisa,
         "caption": "The Simpsons Politiscale",
         "href": "https://the-simpsons-politiscales.fr/"
     },
     {
-        "id": 3,
+        "id": 4,
         "src": MPC,
         "caption": "Mon petit coton - En cours",
         "href": "https://mon-petit-coton.netlify.app/gallery"
     },
     {
-        "id": 4,
+        "id": 5,
         "src": OldCV,
         "caption": "Ancien CV",
         "href": "https://www.linkedin.com/in/guillaume-milleret/"
