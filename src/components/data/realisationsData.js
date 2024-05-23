7e30bd724e0bbb71e9b30cb1fb67bf013e848e6a
Guimill <86968380+Guimill@users.noreply.github.com>
2024-05-23 14:16:22 +0200

    Update realisationsData.js

--- src/components/data/realisationsData.js
+++ src/components/data/realisationsData.js
@@ -3,8 +3,44 @@ import shinyZH from '../../assets/img/shiny.png';
 import MPC from '../../assets/img/isla_500x500.54939114_c40i6j8i.webp';
 import OldCV from '../../assets/img/oldcv.jpeg'
 import ShinyLeaflet from '../../assets/img/shinyLeaflet.png'
+import Angers from '../../assets/img/Angers.png'
+import Spectrum from '../../assets/img/Spectrum.png'
+import Regex from '../../assets/img/Regex.png'
+import API from '../../assets/img/API.png'
+import OSCAR from '../../assets/img/OSCAR.png'
+
 
 export const RealisationsData = [
+    {
+        "id": 1,
+        "src": Angers,
+        "caption": "Beautiful Map Designer",
+        "href": "https://pretty-buffered-city-maps.streamlit.app/"
+    },
+    {
+        "id": 2,
+        "src": Spectrum,
+        "caption": "Pokedata Jrose 11 (statistiques pokemon)",
+        "href": "https://pokedata-jrose11.streamlit.app/"
+    },
+    {
+        "id": 3,
+        "src": Regex,
+        "caption": "Just a regex tool",
+        "href": "https://just-a-regex-tool.streamlit.app/"
+    },
+    {
+        "id": 1,
+        "src": OSCAR,
+        "caption": "OSCAR Outil-s-quentiel-de-correction-automatique-de-Refsite",
+        "href": "https://github.com/Guimill/OSCAR---Outil-s-quentiel-de-correction-automatique-de-Refsite"
+    },
+    {
+        "id": 1,
+        "src": API,
+        "caption": "API---Automatic-PIT-Importer",
+        "href": "https://github.com/Guimill/API---Automatic-PIT-Importer"
+    },
     {
         "id": 1,
         "src": ShinyLeaflet,
@@ -20,7 +56,7 @@ export const RealisationsData = [
     {
         "id": 3,
         "src": lisa,
-        "caption": "The Simpsons Politiscale",
+        "caption": "The Simpsons Politiscale - (HS - bd déconnecté)",
         "href": "https://the-simpsons-politiscales.fr/"
     },
     {
